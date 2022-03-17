053b8199c6538eef50747acd2069dc02c1f46e50
Francisco-pixel <64024698+Francisco-pixel@users.noreply.github.com>
2022-03-17 13:17:39 -0400

    Subida de archivos

---
+++ js/config.js
@@ -0,0 +1,15 @@
+import { c, d, w,n} from "https://francisco-pixel.github.io/modulos/js/data.js";
+import { conexionInternet } from "https://francisco-pixel.github.io/modulos/js/conexionInternet.js";
+import { tiempo } from "https://francisco-pixel.github.io/modulos/js/fecha.js";
+import { crearNodo } from "https://francisco-pixel.github.io/modulos/js/crearNodo.js";
+import { loaderStyle } from "https://francisco-pixel.github.io/modulos/js/loader.js";
+import { padreHijo } from "https://francisco-pixel.github.io/modulos/js/padreHijo.js";
+import { setText } from "https://francisco-pixel.github.io/modulos/js/setText.js";
+import { btnUp,btnUpStyle } from "https://francisco-pixel.github.io/modulos/js/botonSubir.js";
+import { copiarText } from "https://francisco-pixel.github.io/modulos/js/copiarText.js";
+import { userAgent } from "https://francisco-pixel.github.io/modulos/js/userAgent.js";
+conexionInternet()
+btnUp()
+tiempo()
+copiarText()
+userAgent()
